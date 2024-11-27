94377a7c3292688f2d2bd8c350377c19ed7bac0e
KauãS4ntiago <159498457+KauaS4ntiago@users.noreply.github.com>
2024-11-27 16:25:54 -0300

    Update Streaming.java

--- conteudo.java
+++ conteudo.java
@@ -1,67 +1,73 @@
-
-public class conteudo {
+public class Streaming {
 	protected String titulo; // Titulo da serie, filme, documentario e etc...
 	protected String genero; // Genero do filme, serie, documentario e etc...
-	protected int classificacaoIndicada; //maiores de 18, para maiores de 16 e etc..
-	protected String idioma; //audio em Portugues, ingles, espanhol e etc...
-	protected String legenda; //lengenda em Portugues, ingles, espanhol e etc...
+	protected String categoria; // Se é filme, serie, documentario e etc...
+	protected int classificacaoIndicada; // maiores de 18, para maiores de 16 e etc...
+	protected String idioma; // audio em Portugues, ingles, espanhol e etc...
+	protected String legenda; // lengenda em Portugues, ingles, espanhol e etc...
 
-    // Construtores
-    public conteudo(){
+	// Construtores
+	public Streaming() {
 
-    }
+	}
 
-    public conteudo(String titulo){
-        this.titulo = titulo;
-    }
+	public Streaming(String titulo, String genero, int classificacaoIndicada, String idioma, String categoria,
+			String legenda) {
+		this.titulo = titulo;
+		this.genero = genero;
+		this.categoria = categoria;
+		this.classificacaoIndicada = classificacaoIndicada;
+		this.idioma = idioma;
+		this.legenda = legenda;
+	}
 
-    public conteudo(String titulo, String genero, int classificacaoIndicada, String idioma, String legenda){
-        this.titulo = titulo;
-        this.genero = genero;
-        this.classificacaoIndicada = classificacaoIndicada;
-        this.idioma = idioma;
-        this.legenda = legenda;
-    }
-    
-    //getters e setters
-    public void setTitulo( String titulo ){
-        this.titulo = titulo;
-    }
+	// getters e setters
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getTitulo(){
-        return this.titulo;
-    }
+	public String getTitulo() {
+		return this.titulo;
+	}
 
-    public void setGenero( String genero ){
-        this.genero = genero;
-    }
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 
-    public String getGenero(){
-        return this.genero;
-    }
+	public String getGenero() {
+		return this.genero;
+	}
 
-    public void setClassificacaoIndicada( int classificacaoIndicada){
-        this.classificacaoIndicada = classificacaoIndicada;
-    }
+	public String getCategoria() {
+		return categoria;
+	}
 
-    public int getClassificacaoIndicada(){
-        return this.classificacaoIndicada;
-    }
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
-    public void setIdioma( String idioma){
-        this.idioma = idioma;
-    }
+	public void setClassificacaoIndicada(int classificacaoIndicada) {
+		this.classificacaoIndicada = classificacaoIndicada;
+	}
 
-    public String getIdioma(){
-        return this.idioma;
-    }
+	public int getClassificacaoIndicada() {
+		return this.classificacaoIndicada;
+	}
 
-    public void setLegenda( String legenda ){
-        this.legenda = legenda;
-    }
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
 
-    public String getLegenda(){
-        return this.legenda;
-    }
+	public String getIdioma() {
+		return this.idioma;
+	}
+
+	public void setLegenda(String legenda) {
+		this.legenda = legenda;
+	}
+
+	public String getLegenda() {
+		return this.legenda;
+	}
 
 }
