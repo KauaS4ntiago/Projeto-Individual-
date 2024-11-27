ef741262041d5333b839d5a44630b5c51a62402a
KauãS4ntiago <159498457+KauaS4ntiago@users.noreply.github.com>
2024-11-27 16:25:20 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -1,28 +1,46 @@
-abstract class pessoa{
+public abstract class Pessoa {
 
-    //dados do usuário
+	// dados do usuário
 
-    protected String nomeCompleto;
-    protected String cpf;
-    protected int idade;
+	protected String nomeCompleto;
+	protected String cpf;
+	public int idade;
 
-    //construtores
+	// construtores
 
-    public pessoa() {
+	public Pessoa() {
 
-    }
+	}
 
-    public pessoa(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
+	public Pessoa(String nomeCompleto, String cpf, int idade) {
+		this.nomeCompleto = nomeCompleto;
+		this.cpf = cpf;
+		this.idade = idade;
+	}
 
-    public pessoa(String nomeCompleto,String cpf,int idade) {
-        this.nomeCompleto = nomeCompleto;
-        this.cpf = cpf;
-        this.idade = idade;
-    }
-    //dados
-    abstract void setCpf(String cpf);
-    abstract void setNomeCompleto(String NomeCompleto);
-    abstract void setIdade(int idade);
+	// métodos getters e setters
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getNomeCompleto() {
+		return this.nomeCompleto;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getIdade() {
+		return this.idade;
+	}
 }
