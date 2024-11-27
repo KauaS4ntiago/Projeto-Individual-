e569b00fe3be106ded8b4ff9a59a499870c4ad7d
KauãS4ntiago <159498457+KauaS4ntiago@users.noreply.github.com>
2024-11-27 16:24:33 -0300

    Update usuario.java

--- usuario.java
+++ usuario.java
@@ -1,80 +1,70 @@
-public class usuario extends pessoa implements Ilogin {
+public class Usuario extends Pessoa implements Ilogin {
 
-    //informações da conta
+	// informações da conta
 
-    protected String nomeDeUsuario;
-    private String email;
-    private String senha;
-    protected String numeroCelular;
+	private String nomeDeUsuario;
+	private String email;
+	private String senha;
+	private String numeroCelular;
 
-    //construtor vazio
+	// construtores
 
-    public usuario() {
+	public Usuario() {
 
-    }
+	}
 
-    //construtores
+	public Usuario(String nomeDeUsuario, String senha) {
+		this.nomeDeUsuario = nomeDeUsuario;
+		this.senha = senha;
+	}
 
-    public usuario(String nomeDeUsuario) {
+	public Usuario(String nomeDeUsuario, String email, String senha, String numeroCelular, String nomeCompleto,
+			String cpf, int idade) {
+		super(nomeCompleto, cpf, idade);
+		this.nomeDeUsuario = nomeDeUsuario;
+		this.email = email;
+		this.senha = senha;
+		this.numeroCelular = numeroCelular;
+	}
 
-        this.nomeDeUsuario = nomeDeUsuario;
-    }
+	// getters e setters
 
-    public usuario(String nomeDeUsuario,String email,String senha,String numeroCelular,String nomeCompleto,String cpf,int idade) {
-        super(nomeCompleto, cpf, idade);
-        this.nomeDeUsuario = nomeDeUsuario;
-        this.email = email;
-        this.senha = senha;
-        this.numeroCelular = numeroCelular;
-    }
+	public void setNomeUser(String nomeDeUsuario) {
+		this.nomeDeUsuario = nomeDeUsuario;
+	}
 
-    //getters e setters
+	public String getNomeUser() {
+		return this.nomeDeUsuario;
+	}
 
-    public void setNomeDeUsuario(String nomeDeUsuario) {
-        this.nomeDeUsuario = nomeDeUsuario;
-    }
-    public String getNomeDeUsuario() {
-        return this.nomeDeUsuario;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getEmail() {
-        return this.email;
-    }
+	public String getEmail() {
+		return this.email;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    public String getSenha() {
-        return this.senha;
-    }
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-    public String getNomeCompleto() {
-        return this.nomeCompleto;
-    }
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getCpf() {
-        return this.cpf;
-    }
+	public void setNumeroCelular(String numeroCelular) {
+		this.numeroCelular = numeroCelular;
+	}
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public int getIdade() {
-        return this.idade;
-    }
-    public void setNumeroCelular(String numeroCelular) {
-        this.numeroCelular = numeroCelular;
-    }
-    public String getNumeroCelular() {
-        return this.numeroCelular;
-    }
+	public String getNumeroCelular() {
+		return this.numeroCelular;
+	}
+
+	public boolean autenticao(String nomeDeUsuario, String senha) {
+		if (this.nomeDeUsuario.equals(nomeDeUsuario) && this.senha.equals(senha)) {
+			System.out.println("Login efetuado! Bem vindo de volta " + nomeDeUsuario);
+				return true;
+		} else {
+			System.out.println("Login não efetuado! Certifique-se de que as informações estejam corretas.");
+				return false;
+		}
+	}
 
 }
